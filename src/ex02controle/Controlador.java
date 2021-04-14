11fe3e7371ac6adcab4c309d76d8dd4cb1ea1412
Pri Nitta <egashira.sayuri@gmail.com>
2021-04-14 00:16:06 -0300

    encapsulation class finished

--- src/ex02controle/Controlador.java
+++ src/ex02controle/Controlador.java
@@ -6,7 +6,9 @@ public interface Controlador {
     public abstract void abrirMenu();
     public abstract void fecharMenu();
     public abstract void maisVolume();
+    public abstract void menosVolume();
     public abstract void ligarMudo();
+    public abstract void desligarMudo();
     public abstract void play();
     public abstract void pause();
 
--- src/ex02controle/ControleRemoto.java
+++ src/ex02controle/ControleRemoto.java
@@ -41,42 +41,69 @@ public class ControleRemoto implements Controlador {
     //a classe implementa a interface
     @Override
     public void ligar() {
-
+        this.setLigando(true);
     }
 
     @Override
     public void desligar() {
-
+        this.setLigando(false);
     }
 
     @Override
     public void abrirMenu() {
-
+        System.out.println("Está ligado? " + this.getLigando());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.println("Volume: " + this.getVolume());
+        for (int i = 0; i <= this.getVolume(); i += 10) {
+            System.out.println("[]");
+        }
     }
 
     @Override
     public void fecharMenu() {
-
+        System.out.println("Fechando menu...");
     }
 
     @Override
     public void maisVolume() {
+        if (this.getLigando()) {
+            this.setVolume(this.getVolume() + 5);
+        }
+    }
 
+    @Override
+    public void menosVolume() {
+        if (this.getLigando()) {
+            this.setVolume(this.getVolume() - 5);
+        }
     }
 
     @Override
     public void ligarMudo() {
+        if (this.getLigando() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
 
+    @Override
+    public void desligarMudo() {
+        if (this.getLigando() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }
     }
 
     @Override
     public void play() {
-
+        if (this.getLigando() && !(this.getTocando())) {
+            this.setTocando(true);
+        }
     }
 
     @Override
     public void pause() {
-
+        if (this.getLigando() && this.getTocando()){
+            this.setTocando(false);
+        }
     }
 
 }
